777ca1b10064fc3006f88fb555b60a9a7246117f
3ke4y <282805370@qq.com>
2016-12-20 16:38:58 +0800

    完成首页显示

--- src/api/Server.java
+++ src/api/Server.java
@@ -19,7 +19,7 @@ public class Server {
 	}
 		//17
 
-	public static String serverAddress = "http://172.27.0.5:8080/membercenter/";
+	public static String serverAddress = "http://172.27.0.17:8080/membercenter/";
 
 
 	// 通过 公开 getsharedClient类提供给其他类使用
