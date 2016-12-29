5a78aa8d91777b643b8687a9119ffa3d3b4363fb
136147 <472894046@qq.com>
2016-12-29 10:03:30 +0800

    .....

--- src/api/Server.java
+++ src/api/Server.java
@@ -19,7 +19,7 @@ public class Server {
 	}
 
 
-	public static String serverAddress = "http://172.27.15.47:8080/membercenter/";
+	public static String serverAddress = "http://172.27.15.36:8080/membercenter/";
 
 
 
